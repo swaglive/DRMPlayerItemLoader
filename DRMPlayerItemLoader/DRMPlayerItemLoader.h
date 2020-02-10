6ab2f165c5e8fb12ae666b6893dfd2c5893c4cf2
Peter <peter@swag.live>
2020-02-10 13:20:53 +0800

    Initial Commit

---
+++ DRMPlayerItemLoader/DRMPlayerItemLoader.h
@@ -0,0 +1,19 @@
+//
+//  DRMPlayerItemLoader.h
+//  DRMPlayerItemLoader
+//
+//  Created by peter on 2020/2/10.
+//  Copyright © 2020 SWAG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DRMPlayerItemLoader.
+FOUNDATION_EXPORT double DRMPlayerItemLoaderVersionNumber;
+
+//! Project version string for DRMPlayerItemLoader.
+FOUNDATION_EXPORT const unsigned char DRMPlayerItemLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DRMPlayerItemLoader/PublicHeader.h>
+
+
